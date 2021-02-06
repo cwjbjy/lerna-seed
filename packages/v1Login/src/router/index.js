709b55909989e060c267faae8b8f83b140fb81a7
wj.cao <595842570@qq.com>
2021-02-06 16:55:39 +0800

    diyic

--- packages/v1Login/src/router/index.js
+++ packages/v1Login/src/router/index.js
@@ -6,12 +6,12 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/module2_home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
+    path: '/module2_about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
