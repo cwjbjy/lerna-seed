994e29c2c80b9ab940c5762d3fb6db4a9dfa81aa
wj.cao <595842570@qq.com>
2021-02-06 16:55:39 +0800

    diyic

--- packages/v1Login/src/App.vue
+++ packages/v1Login/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/module2_home">Home</router-link> |
+      <router-link to="/module2_about">About</router-link>
     </div>
     <router-view/>
   </div>
